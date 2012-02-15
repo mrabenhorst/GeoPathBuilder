cd146908df641c4ad86c8e07637928123921aa19
Mark Rabenhorst <mrabenhorst@internationalmapping.com>
2012-02-15 08:21:23 -0500

    Added getCollectionFromGPXFile: using the GPGPXLoader class and changed code to work with the new GPUtilities createTagWithName:attributeVals:attributeKeys:andValue: selector

--- GeoPathBuilder/GPCollection.m
+++ GeoPathBuilder/GPCollection.m
@@ -11,6 +11,7 @@
 #import "GPConstants.h"
 #import "GPWaypoint.h"
 #import "GPRoute.h"
+#import "GPGPXLoader.h"
 
 @implementation GPCollection 
 
@@ -76,17 +77,20 @@
     NSMutableString *GPXDataString = [[[NSMutableString alloc] initWithString:@""] autorelease];
     
     // If attribute exists, add the tag. If not, don't do anything
-    [self name] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_NAME attributes:nil andValue:self.name useCDATA:FALSE]] : nil;
-    [self description] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_DESCRIPTION attributes:nil andValue:self.description useCDATA:TRUE]] : nil;
-    [self author] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_AUTHOR attributes:nil andValue:self.author useCDATA:FALSE]] : nil;
-    [self email] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_EMAIL attributes:nil andValue:self.email useCDATA:FALSE]] : nil;
-    [self url] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_URL attributes:nil andValue:self.url useCDATA:FALSE]] : nil;
-    [self urlname] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_URLNAME attributes:nil andValue:self.urlname useCDATA:FALSE]] : nil;
-    [self time] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_TIME attributes:nil andValue:[GPUtilities dateToGPXFormat:self.time] useCDATA:FALSE]] : nil;
-    [self keywords] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_KEYWORDS attributes:nil andValue:self.keywords useCDATA:FALSE]] : nil;
+    [self name] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_NAME attributeVals:nil attributeKeys:nil andValue:self.name useCDATA:FALSE]] : nil;
+    [self description] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_DESCRIPTION attributeVals:nil attributeKeys:nil andValue:self.description useCDATA:TRUE]] : nil;
+    [self author] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_AUTHOR attributeVals:nil attributeKeys:nil andValue:self.author useCDATA:FALSE]] : nil;
+    [self email] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_EMAIL attributeVals:nil attributeKeys:nil andValue:self.email useCDATA:FALSE]] : nil;
+    [self url] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_URL attributeVals:nil attributeKeys:nil andValue:self.url useCDATA:FALSE]] : nil;
+    [self urlname] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_URLNAME attributeVals:nil attributeKeys:nil andValue:self.urlname useCDATA:FALSE]] : nil;
+    [self time] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_TIME attributeVals:nil attributeKeys:nil andValue:[GPUtilities dateToGPXFormat:self.time] useCDATA:FALSE]] : nil;
+    [self keywords] ? [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_KEYWORDS attributeVals:nil attributeKeys:nil andValue:self.keywords useCDATA:FALSE]] : nil;
     // If bounds are all 0, bounds haven't been set
     if( !(self.bounds.north == 0 && self.bounds.south == 0 && self.bounds.east == 0 && self.bounds.west == 0) ) {
-        [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_BOUNDS attributes:nil andValue:[GPUtilities GPBoundsToGPXFormat:[self bounds]] useCDATA:FALSE]];
+        
+        NSArray *values = [NSArray arrayWithObjects:[NSString stringWithFormat:@"%f",self.bounds.north],[NSString stringWithFormat:@"%f",self.bounds.south],[NSString stringWithFormat:@"%f",self.bounds.east],[NSString stringWithFormat:@"%f",self.bounds.west], nil];
+        NSArray *keys = [NSArray arrayWithObjects:@"minlat", @"minlon", @"maxlat", @"maxlon", nil];
+        [GPXDataString appendString:[GPUtilities createTagWithName:kGPXTAG_BOUNDS attributeVals:values attributeKeys:keys andValue:@"" useCDATA:FALSE]];
     } 
     
     // Get the GPX String of each route
@@ -104,15 +108,34 @@
         [GPXDataString appendString:[((GPRoute*)[tracks objectAtIndex:i]) getGPXString]];
     }
     
-    NSDictionary *gpxAttributes = [NSDictionary dictionaryWithObjectsAndKeys:kGPX_SCHEMA_VERSION,@"Version",self.creator,@"Creator",kGPX_XSI_VAL,kGPX_XSI_ATTR,kGPX_XMLNS_VAL,kGPX_XMLNS_ATTR,kGPX_SCHEMA_LOC_VAL,kGPX_SCHEMA_LOC_ATTR, nil];
+    NSArray *gpxAttributeVals = [NSArray arrayWithObjects:kGPX_SCHEMA_VERSION,self.creator,kGPX_XSI_VAL,kGPX_XMLNS_VAL,kGPX_SCHEMA_LOC_VAL, nil];
+    NSArray *gpxAttributeKeys = [NSArray arrayWithObjects:@"Version",@"Creator",kGPX_XSI_ATTR,kGPX_XMLNS_ATTR,kGPX_SCHEMA_LOC_ATTR, nil];
     
     // Add the tag data into the main tag 
     NSString *xmlHeaderTag = [NSString stringWithString:@"<?xml version=\"1.0\"?>"];
-    NSString *GPXTagString = [NSString stringWithFormat:@"%@%@",xmlHeaderTag, [GPUtilities createTagWithName:kGPXTAG_MASTER attributes:gpxAttributes andValue:GPXDataString useCDATA:FALSE]];
+    NSString *GPXTagString = [NSString stringWithFormat:@"%@%@",xmlHeaderTag, [GPUtilities createTagWithName:kGPXTAG_MASTER attributeVals:gpxAttributeVals attributeKeys:gpxAttributeKeys andValue:GPXDataString useCDATA:FALSE]];
     
     return GPXTagString;
 }
-                     
-                     
+
++(id)getCollectionFromGPXFile: (NSData*) gpxFileData {
+    
+    NSXMLParser *nsXmlParser = [[NSXMLParser alloc] initWithData:gpxFileData];
+    GPGPXLoader *gpxLoader = [[GPGPXLoader alloc] init];
+    
+    [nsXmlParser setDelegate:gpxLoader];
+    
+    BOOL success = [nsXmlParser parse];
+    if( success ) {
+        
+        // get data from gpxLoader
+        return [gpxLoader getCollection];
+    } else {
+        
+        // something very bad happened...
+        return nil;
+    }
+
+}
 
 @end
