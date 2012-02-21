88fd50c0d0c2dafa47ebc0ed00e8b905691f3c35
Mark Rabenhorst <mrabenhorst@internationalmapping.com>
2012-02-21 16:47:50 -0500

    Added GPKMLPlacemark class to help with importing KML files

---
+++ GeoPathBuilder/KML Support/GPKMLPlacemark.h
@@ -0,0 +1,18 @@
+//
+//  GPKMLPlacemark.h
+//  GeoPathBuilder
+//
+//  Created by Mark Rabenhorst on 2/21/12.
+//  Copyright (c) 2012 Intl Mapping. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPKMLPlacemark : NSObject {
+    
+}
+
+@property (nonatomic, retain) NSString *name;
+@property (nonatomic, retain) NSString *description;
+
+@end
---
+++ GeoPathBuilder/KML Support/GPKMLPlacemark.m
@@ -0,0 +1,24 @@
+//
+//  GPKMLPlacemark.m
+//  GeoPathBuilder
+//
+//  Created by Mark Rabenhorst on 2/21/12.
+//  Copyright (c) 2012 Intl Mapping. All rights reserved.
+//
+
+#import "GPKMLPlacemark.h"
+
+@implementation GPKMLPlacemark
+
+@synthesize name;
+@synthesize description;
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
